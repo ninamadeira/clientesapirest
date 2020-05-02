112a4cf7d3c4a57a41b023defcd01b8613bcc120
ninamadeira <nina.madeira@gmail.com>
2020-05-02 11:12:22 -0300

    alteração do sabado

--- src/main/java/com/clientes/clientesapirest/domain/service/ClienteService.java
+++ src/main/java/com/clientes/clientesapirest/domain/service/ClienteService.java
@@ -8,25 +8,24 @@ import com.clientes.clientesapirest.domain.repository.ClienteRepository;
 
 @Service
 public class ClienteService {
-	
+
 	ClienteRepository clienteRepository;
-	
+
 	public ClienteService(ClienteRepository clienteRepository) {
 		this.clienteRepository = clienteRepository;
 	}
 
 	public Cliente salvar(Cliente cliente) {
-		Cliente clienteExistente = clienteRepository.findByCpf(cliente.getCpf());	
+		Cliente clienteExistente = clienteRepository.findByCpf(cliente.getCpf());
 		if (clienteExistente != null && !clienteExistente.equals(cliente)) {
 			throw new NegocioException("Já existe um cliente cadastrado com este cpf.");
 		}
-		
+
 		return clienteRepository.save(cliente);
 	}
-	
+
 	public void excluir(Long clienteId) {
 		clienteRepository.deleteById(clienteId);
 	}
-	
 
 }
